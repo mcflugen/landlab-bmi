705b5125dcf899d27ddf554fa58e1a51c1f3cadf
mcflugen <mcflugen@gmail.com>
2025-05-11 10:37:00 -0600

    add _constants.py

---
+++ src/landlab_bmi/_constants.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+BMI_LOCATION = {
+    "node": "node",
+    "link": "edge",
+    "patch": "face",
+    "corner": "node",
+    "face": "edge",
+    "cell": "face",
+    "grid": "none",
+}
+
+BMI_GRID = {
+    "node": 0,
+    "link": 0,
+    "patch": 0,
+    "corner": 1,
+    "face": 1,
+    "cell": 1,
+    "grid": 2,
+}
