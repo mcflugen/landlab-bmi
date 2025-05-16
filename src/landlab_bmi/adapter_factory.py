73b6b76d98ad0df533cfc70bf5b2105fcba5e2b9
mcflugen <mcflugen@gmail.com>
2025-05-16 13:01:36 -0600

    add docstring from make_bmi

--- src/landlab_bmi/adapter_factory.py
+++ src/landlab_bmi/adapter_factory.py
@@ -8,6 +8,20 @@ from landlab_bmi._landlab import BmiGridManager
 
 
 def create_bmi_adapter(class_name: str, component: type[Component]) -> type[Bmi]:
+    """Dynamically create a new BMI-compatible class for a given Landlab Component.
+
+    Parameters
+    ----------
+    class_name : str
+        The name of the generated BMI class.
+    component : type[Component]
+        The Landlab Component class to be wrapped.
+
+    Returns
+    -------
+    type[Bmi]
+        A new class inheriting from LandlabBmi, wrapping the provided component.
+    """
     return type(class_name, (LandlabBmi,), {"_cls": component})
 
 
